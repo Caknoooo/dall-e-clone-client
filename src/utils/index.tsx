3d8faf5f115b12fbad92238e8451989c304fcdac
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-01 17:40:37 +0700

    feat: add utils for random generate

---
+++ src/utils/index.tsx
@@ -0,0 +1,12 @@
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt:string): string {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt)
+    return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}

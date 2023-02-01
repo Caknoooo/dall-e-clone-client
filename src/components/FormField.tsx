11352d372f6c6803385038389690d39470d47f3f
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-01 17:39:35 +0700

    feat: add form for create post

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
 
-const FormField = () => {
+type fieldSubmit = {
+  labelName: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  handleChange: void;
+  isSurpriseMe?: boolean;
+  handleSurpriseMe?: void;
+};
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}: fieldSubmit) => {
   return (
     <div>
-      Test
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded=[5px] text-black"
+          >
+            SurpriseMe
+          </button>
+        )}
+      </div>
+      <input type={type} id={name} name={name} placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray border border-gray text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default FormField
+export default FormField;

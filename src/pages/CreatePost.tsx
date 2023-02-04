ee1c9f4ebc51fd6c40220fc96b6e8ad8712293a9
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-04 16:43:25 +0700

    fix: fixed fetch api with https

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -56,13 +56,16 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ prompt: form.prompt })
-        })
+        const response = await fetch(
+          "https://dall-e-clone-server-ruby.vercel.app/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
         const data = await response.json();
         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
       } catch (error) {
